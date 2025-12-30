dfb6f13a987380ce4e8f9086b514bb6598b6ba3c
isticism <splinecraft.anim@gmail.com>
2025-12-30 12:01:43 -0800

    lane updates

--- assets/lane.gd
+++ assets/lane.gd
@@ -13,7 +13,7 @@ var bodies_in_lane : Array[Node2D] = []
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
-
+	
 func _on_body_entered(body):
 	bodies_in_lane.append(body)
 	
--- frogger_gym.gd
+++ frogger_gym.gd
@@ -3,31 +3,44 @@ extends Node2D
 @onready var frog: CharacterBody2D = $Frog
 @onready var death_area: Area2D = $DeathArea
 @onready var safe_area: Area2D = $SafeArea
-@onready var mover: Mover = $Mover
+@onready var road_01: Area2D = %Road_01
+@onready var road_02: Area2D = %Road_02
+@onready var road_03: Area2D = %Road_03
+@onready var road_04: Area2D = %Road_04
+@onready var road_05: Area2D = %Road_05
+@onready var water_01: Area2D = %Water_01
+@onready var water_02: Area2D = %Water_02
+@onready var water_03: Area2D = %Water_03
+@onready var water_04: Area2D = %Water_04
+@onready var water_05: Area2D = %Water_05
 
-@onready var lane: Area2D = $Lane
+@onready var lanes : Array[Area2D] = [
+	road_01,
+	road_02,
+	road_03,
+	road_04,
+	road_05,
+	water_01,
+	water_02,
+	water_03,
+	water_04,
+	water_05
+]
 
 var in_water : bool = false
 var on_platform : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	death_area.body_entered.connect(_in_water.bind(true))
-	death_area.body_exited.connect(_in_water.bind(false))
-	safe_area.body_entered.connect(_on_platform.bind(true))
-	safe_area.body_exited.connect(_on_platform.bind(false))
-	mover.safe_area.body_entered.connect(_on_platform.bind(true))
-	mover.safe_area.body_exited.connect(_on_platform.bind(false))
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if in_water and not on_platform:
 		print("dead!")
-	lane.apply_lane_motion(delta)
+	for lane in lanes:
+		lane.apply_lane_motion(delta)
 	
-		
-
 
 func _in_water(body, state) -> void:
 	in_water = state
