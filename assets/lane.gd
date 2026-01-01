901bcf5e981b8f2cd4435156fcad1e3fb50c3032
splinecraft <splinecraft.anim@gmail.com>
2026-01-01 15:23:26 -0800

    implement alt pattern spawning, all log types

--- assets/lane.gd
+++ assets/lane.gd
@@ -8,11 +8,18 @@ signal spawn_tick
 @export var carries_frog: bool = false
 @export var traffic_pattern: TrafficPattern
 
+# for spawning object/gap where gap can be different size than object
+# otherwise uses pattern string from traffic pattern resource
+@export var use_alt_pattern: bool = false 
+@export var alt_pattern_gap: float = 0.0 # if 0 will be set to same distance as object
+
+
 @onready var spawn_position: Marker2D = %SpawnPosition
 
 var bodies_in_lane : Array[Node2D] = []
 var pattern_index: int = 0
 var distance_accum: float = 0.0
+var alt_pattern: bool = false
 
 
 func _ready() -> void:
@@ -20,6 +27,17 @@ func _ready() -> void:
 	body_exited.connect(_on_body_exited)
 	spawn_tick.connect(_on_lane_spawn_tick)
 	spawn_position.position = spawn_pos
+	if use_alt_pattern:
+		_setup_alt_pattern()
+	
+
+func _setup_alt_pattern():
+	# alt pattern is for cases where we want simple alternating of object/gap, but the gap is a 
+	# custom size rather than the same length of the object
+	if alt_pattern_gap == 0.0:
+			alt_pattern_gap = traffic_pattern.spacing_pixels
+	alt_pattern = true	# on/off switch for if we're on an object or a gap
+	traffic_pattern.pattern = "C" # logic for alt pattern takes care of gaps so no need to define
 	
 	
 func _physics_process(delta: float) -> void:
@@ -28,10 +46,27 @@ func _physics_process(delta: float) -> void:
 		
 	distance_accum += speed * delta
 	
+	if use_alt_pattern:
+		if alt_pattern:
+			if check_distance_accum():
+				alt_pattern = not alt_pattern
+		else:
+			if distance_accum >= alt_pattern_gap:
+				distance_accum -= alt_pattern_gap
+				alt_pattern = not alt_pattern
+	else:
+		check_distance_accum()
+
+
+func check_distance_accum() -> bool:
 	if distance_accum >= traffic_pattern.spacing_pixels:
-		distance_accum -= traffic_pattern.spacing_pixels
-		emit_signal("spawn_tick")	
-	
+			distance_accum -= traffic_pattern.spacing_pixels
+			emit_signal("spawn_tick")
+			return true
+	else:
+		return false
+
+
 func _on_lane_spawn_tick() -> void:
 	var char = traffic_pattern.pattern[pattern_index]
 	
@@ -45,16 +80,20 @@ func _on_lane_spawn_tick() -> void:
 		else:
 			set_physics_process(false)	
 	
+	
 func spawn_object():
 	var object = traffic_pattern.object_scene.instantiate()
 	add_child(object)
 	object.global_position = Vector2(spawn_position.global_position)
 	
+	
 func _on_body_entered(body):
 	bodies_in_lane.append(body)
 	
+	
 func _on_body_exited(body):
 	bodies_in_lane.erase(body)
+	
 	
 func apply_lane_motion(delta):
 	if speed == 0:
