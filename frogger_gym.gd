c542a98135815ea69752ab13f8afe2f90ff8aed5
splinecraft <splinecraft.anim@gmail.com>
2026-01-01 18:15:43 -0800

    added turtles, lane naming update

--- frogger_gym.gd
+++ frogger_gym.gd
@@ -1,28 +1,29 @@
 extends Node2D
 
 @onready var frog: CharacterBody2D = $Frog
-@onready var road_01: Area2D = %Road_01
-@onready var road_02: Area2D = %Road_02
-@onready var road_03: Area2D = %Road_03
-@onready var road_04: Area2D = %Road_04
-@onready var road_05: Area2D = %Road_05
-@onready var water_01: Area2D = %Water_01
-@onready var water_02: Area2D = %Water_02
-@onready var water_03: Area2D = %Water_03
-@onready var water_04: Area2D = %Water_04
-@onready var water_05: Area2D = %Water_05
+@onready var road_buggy: Area2D = %Road_Buggy
+@onready var road_tractor: Area2D = %Road_Tractor
+@onready var road_sedan: Area2D = %Road_Sedan
+@onready var road_racecar: Area2D = %Road_Racecar
+@onready var road_semi: Area2D = %Road_Semi
+@onready var water_turtle_trio: Area2D = %Water_TurtleTrio
+@onready var water_small_log: Area2D = %Water_SmallLog
+@onready var water_large_log: Area2D = %Water_LargeLog
+@onready var water_turtle_duo: Area2D = %Water_TurtleDuo
+@onready var water_med_log: Area2D = %Water_MedLog
+
 
 @onready var lanes : Array[Area2D] = [
-	road_01,
-	road_02,
-	road_03,
-	road_04,
-	road_05,
-	water_01,
-	water_02,
-	water_03,
-	water_04,
-	water_05
+	road_buggy,
+	road_tractor,
+	road_sedan,
+	road_racecar,
+	road_semi,
+	water_turtle_trio,
+	water_small_log,
+	water_large_log,
+	water_turtle_duo,
+	water_med_log
 ]
 
 var in_water : bool = false
